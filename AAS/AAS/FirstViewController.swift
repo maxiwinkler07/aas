e31619457d54697020111478be96055001dede07
Maxi Winkler <maxiwinkler@Maxis-iMac.local>
2015-10-18 18:10:40 +0200

    V1
    Version 1 of the app

---
+++ AAS/AAS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AAS
+//
+//  Created by Maxi Winkler on 18/10/15.
+//  Copyright © 2015 Informaticamaxi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
